49208aff9e4e28e92f9f2fdeffc3cbc8132b134e
jonathan bleibdrey <jonbleibdrey@aol.com>
2023-02-06 22:26:51 -0500

    c++ math function

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() 
+{
+
+  int age = 30;
+
+
+
+  //REMEMBER ODER OF OPERATIONS Please excuse my dear aunt sally. multi and division will come before sub and additon 
+  //math operators plus, divide, multiply
+  cout << 5+7 << endl;
+  cout << 5/7  << endl;
+  cout << 5*7  << endl;
+  //remainder
+  cout << 10 % 3 << endl;
+  cout << (4 + 5) * 10 << endl;
+  //
+
+
+  
+  
+
+
+  return 0;
+  
+} 
