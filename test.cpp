9a104e3eab8ca36a3fc34255a9a19eb71e713a10
jonathan bleibdrey <jonbleibdrey@aol.com>
2023-02-07 19:27:05 -0500

    getting input from user

--- test.cpp
+++ test.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
 
+//Math functions addon 
+#include <cmath>
+
+
 using namespace std;
 
 
 int main() 
 {
 
-  int age = 30;
-
-
-
-  //REMEMBER ODER OF OPERATIONS Please excuse my dear aunt sally. multi and division will come before sub and additon 
-  //math operators plus, divide, multiply
-  cout << 5+7 << endl;
-  cout << 5/7  << endl;
-  cout << 5*7  << endl;
-  //remainder
-  cout << 10 % 3 << endl;
-  cout << (4 + 5) * 10 << endl;
-  //
-
-
+  //to get a number back from the user
+  int age;
   
-  
+  cout << "Enter your age: ";
+  cin >> age;
+ 
 
+  cout << "you are " << age << " years old.";
+
+  //to get a a string back from the user
+  string name;
+
+  cout << "Enter your name: " ;
+
+  //this is the way to get a string, cin gets a prompt to pop up and name is the variable.
+  getline(cin, name);
+
+  cout << "Hello " << name;
+
+  // you could use it mutiple times like this with catname and just name.
+  string catname;
+
+  cout << "Enter your cat name: " ;
+  getline(cin, catname);
+ 
+
+  cout << "Hello " << name << " and " << catname;
 
   return 0;
   
