1709ffb95fb48e818b1719d805498cc7988790aa
jonathan bleibdrey <jonbleibdrey@aol.com>
2023-02-09 18:12:10 -0500

    if statments

--- test.cpp
+++ test.cpp
@@ -5,22 +5,36 @@
 
 using namespace std;
 
-double cube (double num){
 
-  double result = num * num * num;
-
-  return result;
-
-}
 
 int main() 
 {
   
-  double num;
+  bool isMale = true;
+  bool isTall = false;
 
-  double answer = cube (4.0);
+  // AND operator
+  if(isMale && isTall){
+    cout << "You are a male, and tall";
+    // ! NOT
+  } else if(isMale && !isTall){
 
-  cout << answer << endl;
+    cout << "you are short male";
+
+  } else if (!isMale && isTall) {
+
+    cout << "you are Tall but not male";
+
+  }else {
+    cout << "you are female, and short";
+  }
+
+  // OR operator 
+  // if(isMale || isTall){
+  //   cout << "You are a male, and tall";
+  // } else {
+  //   cout << "you are female, and probably short";
+  // }
 
 
   return 0;
