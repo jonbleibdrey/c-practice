f02f717670ad6854b49c8073620886083b869d6e
jonathan bleibdrey <jonbleibdrey@aol.com>
2023-02-09 18:50:41 -0500

    comparison operators

--- test.cpp
+++ test.cpp
@@ -7,35 +7,57 @@ using namespace std;
 
 
 
+
+int maxNum (int num1, int num2)
+{
+  int result;
+
+// > - greater than, < - less than, >= - greater than equal (greater then the number or exactly that number), <= - lesser than equal , == -  comparison (asking is it the exact same) , != - not equal ( as long as its not equal that number it will return  true)
+
+  if (num1 > num2 ){
+     cout << num1 << " is greater then "<< num2 << endl;
+     result = num1;
+  } else  {
+     cout << num2 << " is grater then "<< num1 << endl;
+     result = num2;
+  } 
+
+  return result;
+}
+
+
+int maxNum3 (int num1, int num2, int num3 )
+{
+  int result;
+
+// > - greater than, < - less than, >= - greater than equal (greater then the number or exactly that number), <= - lesser than equal , == -  comparison (asking is it the exact same) , != - not equal ( as long as its not equal that number it will return  true)
+
+  if (num1 >= num2 && num1 >= num3){
+    result = num1;
+  
+  } else if (num2 >= num1 && num2 >= num3) {
+
+    result = num2;
+
+  } else {
+    result = num3;
+  }
+
+  return result;
+}
+
+
+
 int main() 
 {
   
-  bool isMale = true;
-  bool isTall = false;
+  int final;
+  int final2;
+  
+  final = maxNum(5,7);
+  final2 = maxNum3(5, 7, 35);
 
-  // AND operator
-  if(isMale && isTall){
-    cout << "You are a male, and tall";
-    // ! NOT
-  } else if(isMale && !isTall){
-
-    cout << "you are short male";
-
-  } else if (!isMale && isTall) {
-
-    cout << "you are Tall but not male";
-
-  }else {
-    cout << "you are female, and short";
-  }
-
-  // OR operator 
-  // if(isMale || isTall){
-  //   cout << "You are a male, and tall";
-  // } else {
-  //   cout << "you are female, and probably short";
-  // }
-
+  cout << "The bigger number is " << final << " for maxNum and for maxNum3 it is " << final2  << endl;
 
   return 0;
   
