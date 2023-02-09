25f4741f9f91058ab74ee92f2f41f859553c4800
jonathan bleibdrey <jonbleibdrey@aol.com>
2023-02-08 19:15:57 -0500

    return statements

--- test.cpp
+++ test.cpp
@@ -5,13 +5,22 @@
 
 using namespace std;
 
+double cube (double num){
 
+  double result = num * num * num;
+
+  return result;
+
+}
 
 int main() 
 {
   
+  double num;
 
-  
+  double answer = cube (4.0);
+
+  cout << answer << endl;
 
 
   return 0;
