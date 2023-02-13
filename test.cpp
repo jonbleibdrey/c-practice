2fb1740ea1503d97be0b3526f4b84612edb3cfc2
jonathan bleibdrey <jonbleibdrey@aol.com>
2023-02-13 18:56:31 -0500

    case statements

--- test.cpp
+++ test.cpp
@@ -6,35 +6,56 @@
 using namespace std;
 
 
+string getDayOfWeek (int dayNum) {
+
+  string dayName;
+
+  switch (dayNum) {
+
+    case 0:
+      dayName = "Sunday";
+      break;
+    case 1:
+      dayName = "Monday";
+      break;
+    case 2:
+      dayName = "Tuesday";
+      break;
+    case 3:
+      dayName = "Wednesday";
+      break;
+    case 4:
+      dayName = "Thursday";
+      break;
+    case 5:
+      dayName = "Friday";
+      break;
+    case 6:
+      dayName = "Saturday";
+      break;
+    default:
+      dayName = "You made a wrong choose";
+  }
+
+
+
+  return dayName;
+
+}
+
 
 int main() 
 {
-  
-int num1; 
-int num2;
-char oper;
+  int dayNum;
+  string result;
 
-cout << "Enter first number: ";
-cin >> num1;
-cout << "Enter first number 2: ";
-cin >> num2;
-cout << "Enter first operator: ";
-cin >> oper;
+  cout << "what day is it?";
+  cin >> dayNum;
 
-int result;
+  result = getDayOfWeek(dayNum);
 
-if (oper == '+') {
-  result = num1 + num2;
-} else if(oper == '-'){
-  result = num1 - num2;
-} else if ( oper == '*') {
-  result = num1 * num2;
-} else if ( oper == '/') {
-  result = num1 / num2;
-} else {
-  cout << "wrong input ";
-  }
-cout << result;
+  cout << "The day is " << result << endl; 
+
 
   return 0;
   
