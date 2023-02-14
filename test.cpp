fd1acf44b7cdad6efa2f52984226e4604d5c2776
jonathan bleibdrey <jonbleibdrey@aol.com>
2023-02-13 19:22:46 -0500

    guesing game

--- test.cpp
+++ test.cpp
@@ -6,55 +6,35 @@
 using namespace std;
 
 
-string getDayOfWeek (int dayNum) {
-
-  string dayName;
-
-  switch (dayNum) {
-
-    case 0:
-      dayName = "Sunday";
-      break;
-    case 1:
-      dayName = "Monday";
-      break;
-    case 2:
-      dayName = "Tuesday";
-      break;
-    case 3:
-      dayName = "Wednesday";
-      break;
-    case 4:
-      dayName = "Thursday";
-      break;
-    case 5:
-      dayName = "Friday";
-      break;
-    case 6:
-      dayName = "Saturday";
-      break;
-    default:
-      dayName = "You made a wrong choose";
-  }
-
-
-
-  return dayName;
-
-}
-
 
 int main() 
 {
-  int dayNum;
-  string result;
 
-  cout << "what day is it?";
-  cin >> dayNum;
+  int secret = 7;
+  int guess;
+  int guessCount = 0;
+  int guessLimit = 3;
+  bool outOfGuesses = false;
 
-  result = getDayOfWeek(dayNum);
 
-  cout << "The day is " << result << endl; 
+
+  while(guess != secret  && !outOfGuesses){
+    if (guessCount < guessLimit) {
+          cout << "Enter guess? ";
+          cin >> guess; 
+          guessCount++;
+    }else{
+        outOfGuesses = true;
+    }
+  }
+
+  if(outOfGuesses){
+      cout << "You lose!";
+  } else {
+      cout << "You Win!";
+  }
+  
+
 
 
   return 0;
