1094978558eb08c2dc26822840febbaeec23683c
jonathan bleibdrey <jonbleibdrey@aol.com>
2023-02-14 18:57:48 -0500

    exponent function

--- test.cpp
+++ test.cpp
@@ -7,15 +7,25 @@ using namespace std;
 
 
 
+
+
+int power(int baseNum, int powNum)
+{
+
+  int result = 1;
+
+  for(int i = 0; i < powNum; i++){
+    result = result * baseNum;
+
+  }
+  return result;
+}
+
+
 int main() 
 {
 
-  
-  int nums[] = {1,2,5,7,3};
-  
-  for(int i = 0; i < 5; i++){
-      cout << nums[i] << endl;
-  }
+  cout << power(4,3);
 
 
 
